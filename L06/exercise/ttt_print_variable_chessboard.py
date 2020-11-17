cbd849854713affb007c39779e3c783891666a16
changjing0 <friend920919@gmail.com>
2020-11-17 03:32:43 +0000

    oh ya

--- L06/exercise/ttt_print_variable_chessboard.py
+++ L06/exercise/ttt_print_variable_chessboard.py
@@ -24,3 +24,8 @@ cell_9 = ' '
 #  x |   |   
 
 # TODO: 程式寫這裡 ...
+print(' '+cell_1 + ' | '+ cell_2 +' | '+ cell_3+' ')
+print('---+---+---')
+print(' '+cell_4 + ' | '+ cell_5 +' | '+ cell_6+' ')
+print('---+---+---')
+print(' '+cell_7 + ' | '+ cell_8 +' | '+ cell_9+' ')
--- L06/exercise/ttt_set_center.py
+++ L06/exercise/ttt_set_center.py
@@ -29,3 +29,9 @@ cell_9 = ' '
 #    |   |   
 
 # TODO: 程式寫這裡
+cell_5 = input()
+print(' '+cell_1 + ' | '+ cell_2 +' | '+ cell_3+' ')
+print('---+---+---')
+print(' '+cell_4 + ' | '+ cell_5 +' | '+ cell_6+' ')
+print('---+---+---')
+print(' '+cell_7 + ' | '+ cell_8 +' | '+ cell_9+' ')
